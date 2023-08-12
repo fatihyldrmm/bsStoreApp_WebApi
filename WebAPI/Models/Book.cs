debb367af61ebcd588842326f7ea8dde0b076758
fatih <fatih@FATIH>
2023-08-13 00:36:17 +0300

    Add project files.

---
+++ WebAPI/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
